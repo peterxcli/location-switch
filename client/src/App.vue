0a49c3e4a79698dff7cb4c4722df670deb3f7f63
jason810496 <810496@email.wlsh.tyc.edu.tw>
2023-05-26 16:42:03 +0800

    git init

---
+++ client/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import SideBar from './components/SideBar.vue'
+import MapView from './components/MapView.vue'
+</script>
+
+<template>
+  <div id="AppView">
+    <MapView />
+    <SideBar />
+  </div>
+</template>
+
+<style>
+#AppView {
+  display: flex;
+  justify-content:space-evenly;
+  height: 100vh;
+  width: 100vw;
+}
+</style>
---
+++ client/src/components/MapView.vue
@@ -0,0 +1,45 @@
+<template>
+    <div id="MapView">
+        <div id="map"></div>
+    </div>
+</template>
+  
+<script>
+import { defineComponent } from 'vue';
+import L from 'leaflet';
+
+
+export default defineComponent({
+    data() {
+        return {
+            streetMap: null,
+        }
+    },
+    mounted() {
+        this.streetMap = L.map('map', {
+            center: [25.042474, 121.513729],
+            zoom: 18,
+        });
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 20,
+        }).addTo(this.streetMap);
+    },
+    name: 'MapView',
+    methods: {
+        async func() {
+            // do something
+        }
+    },
+});
+</script>
+  
+<style>
+#MapView {
+    flex: 3 1 0;
+    color: #959595;
+}
+#map { height: 100vh; }
+</style>
+  
