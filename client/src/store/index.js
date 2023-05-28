97bfc694bcc1f378238b3b5f26e97f4d5ba40164
jason810496 <810496@email.wlsh.tyc.edu.tw>
2023-05-28 16:02:03 +0800

    ..

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,11 +1,13 @@
 import { createStore, createLogger } from 'vuex'
 import users from './modules/users'
+import myself from './modules/myself'
 
 const debug = process.env.NODE_ENV !== 'production'
 
 export default createStore({
     modules: {
-        users
+        users,
+        myself
     },
     strict: debug,
     plugins: debug ? [createLogger()] : []
