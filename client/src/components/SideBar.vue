b0d772a1014b160bd6a09cefd12857262ec4a3f2
jason810496 <810496@email.wlsh.tyc.edu.tw>
2023-05-28 15:19:34 +0800

    material design

--- client/src/components/SideBar.vue
+++ client/src/components/SideBar.vue
@@ -6,14 +6,14 @@
     <!-- postion form -->
     <div class="row">
       <div class="icon-col">
-        <svg style="fill: rgb(42, 49, 61);" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M16 144a144 144 0 1 1 288 0A144 144 0 1 1 16 144zM160 80c8.8 0 16-7.2 16-16s-7.2-16-16-16c-53 0-96 43-96 96c0 8.8 7.2 16 16 16s16-7.2 16-16c0-35.3 28.7-64 64-64zM128 480V317.1c10.4 1.9 21.1 2.9 32 2.9s21.6-1 32-2.9V480c0 17.7-14.3 32-32 32s-32-14.3-32-32z"/></svg>
+        <svg style="fill: rgb(63, 79, 104);" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M16 144a144 144 0 1 1 288 0A144 144 0 1 1 16 144zM160 80c8.8 0 16-7.2 16-16s-7.2-16-16-16c-53 0-96 43-96 96c0 8.8 7.2 16 16 16s16-7.2 16-16c0-35.3 28.7-64 64-64zM128 480V317.1c10.4 1.9 21.1 2.9 32 2.9s21.6-1 32-2.9V480c0 17.7-14.3 32-32 32s-32-14.3-32-32z"/></svg>
       </div>
       <div class="form-col">
         <form class="pos-form">
           <label for="lat-input">lat:</label>
-          <input id="lat-input" type="text"/>
+          <input id="lat-input" type="text" placeholder="25.042474"/>
           <label for="lon-input">lon:</label>
-          <input id="lon-input" type="text"/>
+          <input id="lon-input" type="text" placeholder="121.513729"/>
           <button type="button" @click="updatePosition">Go</button>
         </form>
       </div>
@@ -28,11 +28,11 @@
         <form class="check-in-form">
           <label for="url-input">image url:</label>
           <br>
-          <input id="url-input" type="text"/>
+          <input id="url-input" type="text" placeholder="https://img.url.com/321"/>
           <br>
           <label for="content-input">content:</label>
           <br>
-          <input id="content-input" type="text"/>
+          <input id="content-input" type="text" placeholder="check in post ..."/>
           <br>
           <button type="button" @click="updatePosition">check in</button>
         </form>
@@ -42,7 +42,7 @@
     <!-- broadcast form -->
     <div class="row">
       <div class="icon-col">
-        <svg style="fill: rgb(42, 49, 61);" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M256 448c141.4 0 256-93.1 256-208S397.4 32 256 32S0 125.1 0 240c0 45.1 17.7 86.8 47.7 120.9c-1.9 24.5-11.4 46.3-21.4 62.9c-5.5 9.2-11.1 16.6-15.2 21.6c-2.1 2.5-3.7 4.4-4.9 5.7c-.6 .6-1 1.1-1.3 1.4l-.3 .3 0 0 0 0 0 0 0 0c-4.6 4.6-5.9 11.4-3.4 17.4c2.5 6 8.3 9.9 14.8 9.9c28.7 0 57.6-8.9 81.6-19.3c22.9-10 42.4-21.9 54.3-30.6c31.8 11.5 67 17.9 104.1 17.9zM128 208a32 32 0 1 1 0 64 32 32 0 1 1 0-64zm128 0a32 32 0 1 1 0 64 32 32 0 1 1 0-64zm96 32a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z"/></svg>
+        <svg style="fill: rgb(63, 79, 104);height: 40px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M256 448c141.4 0 256-93.1 256-208S397.4 32 256 32S0 125.1 0 240c0 45.1 17.7 86.8 47.7 120.9c-1.9 24.5-11.4 46.3-21.4 62.9c-5.5 9.2-11.1 16.6-15.2 21.6c-2.1 2.5-3.7 4.4-4.9 5.7c-.6 .6-1 1.1-1.3 1.4l-.3 .3 0 0 0 0 0 0 0 0c-4.6 4.6-5.9 11.4-3.4 17.4c2.5 6 8.3 9.9 14.8 9.9c28.7 0 57.6-8.9 81.6-19.3c22.9-10 42.4-21.9 54.3-30.6c31.8 11.5 67 17.9 104.1 17.9zM128 208a32 32 0 1 1 0 64 32 32 0 1 1 0-64zm128 0a32 32 0 1 1 0 64 32 32 0 1 1 0-64zm96 32a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z"/></svg>
       </div>
       <div class="form-col">
         <form class="broadcast-form">
@@ -91,26 +91,45 @@ export default defineComponent({
 }
 #SideBar .icon-col{
   flex: 1 1 0;
-  background-color: aliceblue;
+  background-color: #f2f2f2;
   text-align: center;
 }
 #SideBar .form-col{
   flex: 5 1 0;
-  background-color: rgb(255, 255, 255);
+  background-color: #f2f2f2;
+  padding-left: 5px;
 }
 
+.form-col label{
+  font-family: 'Roboto', sans-serif;
+  font-weight: 500;
+}
 
+.form-col input{
+  height: 20px;
+  border-radius: 5px;
+  border: 1px solid rgb(72, 73, 75);
+  padding: 5px;
+  margin: 5px;
+  font-family: 'Roboto', sans-serif;
+}
+.form-col button{
+  color: #fff;
+  height: 30px;
+  border-radius: 5px;
+  background-color: #353535;
+  border: 1px solid rgb(72, 73, 75);
+  padding: 5px 10px;
+  margin: 5px;
+  font-family: 'Roboto', sans-serif;
+}
 
 .icon-col{
-  display: flex;
-  flex-direction: row;
+  padding-top: 20px;
 }
 .icon-col svg{
   width: 18px;
   margin: 0 auto;
-}
-.form-col{
-  margin-left: 20px;
 }
 
 #SideBar form{
